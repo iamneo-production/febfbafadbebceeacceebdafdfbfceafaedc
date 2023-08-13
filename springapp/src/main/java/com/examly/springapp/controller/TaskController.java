fc67ce5c9489ecc89d21025ec97b3332adc78e04
Iamneo <techadmin@iamneo.ai>
2023-08-13 08:55:55 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/controller/TaskController.java
@@ -2,20 +2,17 @@ package com.examly.springapp.controller;
 
 import java.util.List;
 
+import com.examly.springapp.exception.ResourceNotFoundException;
+import com.examly.springapp.model.Task;
+import com.examly.springapp.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.examly.springapp.exception.ResourceNotFoundException;
-import com.examly.springapp.model.Task;
-import com.examly.springapp.repository.TaskRepository;
 
 @RestController
 @RequestMapping("")
